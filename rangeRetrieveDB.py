b093020c388950505eb57bf63323469dd2170033
liuhhhing <hing.liu@gmail.com>
2021-12-23 23:11:46 +0800

    Final version of the range retriever using DB

--- rangeRetrieveDB.py
+++ rangeRetrieveDB.py
@@ -1,6 +1,9 @@
 import logging
+import threading
+
 from rangeRetrieveInterface import RangeRetrieveInterface
 import sqlite3
+import time
 
 
 class RangeRetrieveDB(RangeRetrieveInterface):
@@ -10,59 +13,112 @@ class RangeRetrieveDB(RangeRetrieveInterface):
         self.conn = None
 
     def get_lock(self, timeout):
+        logging.debug("Start to get lock")
+        success = True
         self.timeout = timeout
-        self.conn = sqlite3.connect(str(self.db_file), detect_types=sqlite3.PARSE_DECLTYPES, timeout=self.timeout)
-        try:
-            with self.conn:
-                self.conn.execute("update ranges set occupied = 1 where id = 0")
-                # this is to lock the db
 
-        except sqlite3.Error as e:
-            logging.debug(e)
-            return False
+        # this is to lock the db
+        count = self.timeout
+        while True:
+            try:
+                self.conn = sqlite3.connect(str(self.db_file), detect_types=sqlite3.PARSE_DECLTYPES, timeout=10)
+                cursor = self.conn.cursor()
+                cursor.execute("update ranges set occupied = 1 where id = 0 and occupied = 0")
+                if cursor.rowcount == 1:
+                    logging.debug("Success to acquire lock")
+                    self.conn.commit()
+                    success = True
+                else:
+                    success = False
 
-        return True
+            except sqlite3.Error as e:
+                logging.debug("Exception on acquire: {0}".format(str(e)))
+                success = False
+
+            self.conn.close()
+            if success:
+                break
+            else:
+                time.sleep(1)
+                logging.debug("retrying acquire lock: {0}/{1}".format(count, timeout))
+                count = count - 1
+                if count < 0:
+                    logging.debug("timeout reached and still cannot acquire the lock")
+                    break
+
+        logging.debug("Acquire lock done {0}".format(success))
+        return success
 
     def get_range(self):
+        logging.debug("Start to get range")
         start = 0
         end = 0
+        success = True
+        all_occupied = False
 
-        try:
-            with self.conn:
-                cursor = self.conn.cursor()
+        while True:
+            try:
+                self.conn = sqlite3.connect(str(self.db_file), detect_types=sqlite3.PARSE_DECLTYPES)
+                with self.conn:
+                    cursor = self.conn.cursor()
 
-                cursor.execute("update ranges set occupied = 1 where id = 0") # this is to lock the db
-                # table with col like
-                # id, start, end, occupied
-                # SELECT id, start, end from ranges where occupied = 0 order by id asc
-                # update ranges set occupied = 1 where id = <last id>
-                cursor.execute(
-                    "SELECT id, start, end from ranges where occupied = 0 and id > 0 order by id asc"
-                )
-                result = cursor.fetchone()
-                if result is not None:
-                    id = result[0]
-                    start = result[1]
-                    end = result[2]
+                    # table with col like
+                    # id, start, end, occupied
+                    # SELECT id, start, end from ranges where occupied = 0 order by id asc
+                    # update ranges set occupied = 1 where id = <last id>
                     cursor.execute(
-                        "update ranges set occupied = 1 where id = {0}".format(id)
+                        "SELECT id, start, end from ranges where occupied = 0 and id > 0 order by id asc"
                     )
-                else:
-                    return start, end, False
+                    result = cursor.fetchone()
+                    if result is not None:
+                        id = result[0]
+                        start = result[1]
+                        end = result[2]
+                        cursor.execute(
+                            "update ranges set occupied = 1 where id = {0}".format(id)
+                        )
+                        self.conn.commit()
+                        logging.debug("Success to get range {0}, {1}".format(start, end))
+                        success = True
+                    else:
+                        logging.debug("No Range available")
+                        all_occupied = True
+                        success = False
 
-        except sqlite3.Error as e:
-            logging.debug(e)
-            return start, end, False
+            except sqlite3.Error as e:
+                logging.debug("Exception on get range: {0}".format(str(e)))
+                success = False
 
-        return start, end, True
+            if success:
+                break
+            if all_occupied:
+                break
+
+        self.conn.close()
+        return start, end, success
 
     def release_lock(self):
-        try:
-            self.conn.commit()
-            self.conn.close()
-        except sqlite3.Error as e:
-            logging.debug(e)
-            return False
+        logging.debug("Start to release lock")
+        success = True
+        while True:
+            try:
+                self.conn = sqlite3.connect(str(self.db_file), detect_types=sqlite3.PARSE_DECLTYPES)
+                with self.conn:
+                    cursor = self.conn.cursor()
+                    cursor.execute("update ranges set occupied = 0 where id = 0 and occupied = 1")
+                    if cursor.rowcount == 0:
+                        logging.debug("Fail to release the lock")
+                        success = False
+                    else:
+                        logging.debug("Success to release the lock")
+                        self.conn.commit()
+                        success = True
+            except sqlite3.Error as e:
+                logging.debug("Exception on release: {0}".format(str(e)))
+                success = False
 
+            if success:
+                break
 
-        return True
+        self.conn.close()
+        return success
--- test_range.py
+++ test_range.py
@@ -3,6 +3,14 @@ from rangeRetrieveInterface import RangeRetrieveInterface
 from rangeRetrieveDB import RangeRetrieveDB
 import sqlite3
 import threading
+import os
+import time
+import logging
+import concurrent.futures
+
+logging.basicConfig(format='%(thread)d %(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
+                            level=logging.DEBUG)
+
 
 def init_db():
     db_connection = sqlite3.connect(str('test_range.db'), detect_types=sqlite3.PARSE_DECLTYPES)
@@ -25,12 +33,13 @@ def init_db():
     try:
         for i in range(0, 200):
             cursor.execute('''insert into ranges (id, start, end, occupied) values ({0}, {1}, {2}, 0)'''.format(
-                i, (i-1)*100 +1, i*100
+                i, (i - 1) * 100 + 1, i * 100
             ))
     except sqlite3.Error as e:
-        print(e)
+        logging.debug(e)
 
     db_connection.commit()
+
 
 def init_db_with_all_occupied():
     db_connection = sqlite3.connect(str('test_range.db'), detect_types=sqlite3.PARSE_DECLTYPES)
@@ -48,36 +57,39 @@ def init_db_with_all_occupied():
             [occupied] INTEGER NOT NULL
             )''')
     except sqlite3.Error as e:
-        print(e)
+        logging.debug(e)
 
     try:
-        for i in range(0, 200):
+        cursor.execute('''insert into ranges (id, start, end, occupied) values (0, -99, 1, 0)''')
+        for i in range(1, 200):
             cursor.execute('''insert into ranges (id, start, end, occupied) values ({0}, {1}, {2}, 1)'''.format(
-                i, (i-1)*100 +1, i*100
+                i, (i - 1) * 100 + 1, i * 100
             ))
     except sqlite3.Error as e:
-        print(e)
+        logging.debug(e)
 
     db_connection.commit()
+
+
 
 class MyTestCase(unittest.TestCase):
     def test_basic_get_range(self):
         init_db()
-        range = RangeRetrieveInterface.register(RangeRetrieveDB)()
-        range.db_file = 'test_range.db'
-        range.get_lock(100)
-        result = range.get_range()
-        print (result[0], result[1], result[2])
-        range.release_lock()
+        rangeObj = RangeRetrieveInterface.register(RangeRetrieveDB)()
+        rangeObj.db_file = 'test_range.db'
+        rangeObj.get_lock(100)
+        result = rangeObj.get_range()
+        print(result[0], result[1], result[2])
+        rangeObj.release_lock()
 
         self.assertEqual(result[0], 1)
         self.assertEqual(result[1], 100)
         self.assertEqual(result[2], True)
 
-        range.get_lock(100)
-        result = range.get_range()
+        rangeObj.get_lock(100)
+        result = rangeObj.get_range()
         print(result[0], result[1], result[2])
-        range.release_lock()
+        rangeObj.release_lock()
 
         self.assertEqual(result[0], 101)
         self.assertEqual(result[1], 200)
@@ -86,14 +98,45 @@ class MyTestCase(unittest.TestCase):
     def test_all_occupied(self):
         init_db_with_all_occupied()
 
-        range = RangeRetrieveInterface.register(RangeRetrieveDB)()
-        range.db_file = 'test_range.db'
-        range.get_lock(100)
-        result = range.get_range()
-        print(result[0], result[1], result[2])
-        range.release_lock()
+        rangeObj = RangeRetrieveInterface.register(RangeRetrieveDB)()
+        rangeObj.db_file = 'test_range.db'
+        if rangeObj.get_lock(100):
+            result = rangeObj.get_range()
+            print(result[0], result[1], result[2])
+            rangeObj.release_lock()
 
         self.assertEqual(result[2], False)
+
+    def test_lock_timeout(self):
+        init_db()
+        range1 = RangeRetrieveInterface.register(RangeRetrieveDB)()
+        range2 = RangeRetrieveInterface.register(RangeRetrieveDB)()
+
+        range1.db_file = 'test_range.db'
+        range2.db_file = 'test_range.db'
+
+
+        def thread_func_1():
+            range1.get_lock(5)
+            time.sleep(5) # intend not to release lock
+
+        def thread_func_2():
+            time.sleep(1)
+            result = range2.get_lock(5) # it should have timeout
+            return result
+
+        result = True
+        c1 = threading.Thread(target=thread_func_1)
+        c1.start()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            future = executor.submit(thread_func_2)
+            result = future.result()
+
+
+        c1.join()
+
+
+        self.assertEqual(result, False)
 
     def test_get_multiple(self):
         init_db()
@@ -104,18 +147,18 @@ class MyTestCase(unittest.TestCase):
         range2.db_file = 'test_range.db'
 
         def thread_func_1():
-            for i in range(1, 100):
-                range1.get_lock(100)
-                result = range1.get_range()
-               # print("From 1 {0}, {1}, {2}".format(result[0], result[1], result[2]))
-                range1.release_lock()
+            for i in range(1, 5):
+                if range1.get_lock(10) == True:
+                    result = range1.get_range()
+                    # print("From 1 {0}, {1}, {2}".format(result[0], result[1], result[2]))
+                    range1.release_lock()
 
         def thread_func_2():
-            for i in range(1, 100):
-                range2.get_lock(100)
-                result = range2.get_range()
-               # print("From 2 {0}, {1}, {2}".format(result[0], result[1], result[2]))
-                range2.release_lock()
+            for i in range(1, 5):
+                if range2.get_lock(10) == True:
+                    result = range2.get_range()
+                    # print("From 2 {0}, {1}, {2}".format(result[0], result[1], result[2]))
+                    range2.release_lock()
 
         c1 = threading.Thread(target=thread_func_1)
         c2 = threading.Thread(target=thread_func_2)
@@ -130,7 +173,7 @@ class MyTestCase(unittest.TestCase):
         cursor = conn.cursor()
         cursor.execute("select count(*) from ranges where id > 0 and occupied = 1")
         result = cursor.fetchone()
-        self.assertEqual(result[0], 99+99)
+        self.assertEqual(result[0], 8)
 
 
 if __name__ == '__main__':
